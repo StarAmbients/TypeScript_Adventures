183ff779426b80a5a2678f27204e62a2f284ad09
Debora Schuch da Rosa <StarAmbients@gmail.com>
2021-12-26 17:08:43 +0100

    Literal types

--- understandingTS/app.ts
+++ understandingTS/app.ts
@@ -1,3 +1,5 @@
+// union types
+/* 
 function combine (input1: number | string, input2: number | string) {
     let result;
     if (typeof input1 === 'number' && typeof input2 === 'number') {
@@ -8,13 +10,33 @@ function combine (input1: number | string, input2: number | string) {
     return result;
 }
 
-// union types
-
 const combinedAges = combine(30, 26);
 console.log(combinedAges);
 
 const combinedNames = combine('Sarah', 'Richard');
-console.log(combinedNames);
+console.log(combinedNames); */
 
 // literal types
+function combine (input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text' ) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
+    } else {
+            result = input1.toString() + input2.toString();
+    }  
+    return result;
+/*     if (resultConversion === 'as-number') {
+        return +result;
+    } else {
+        return result.toString();
+    } */
+}
 
+const combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+
+const combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine('Sarah', 'Richard', 'as-text');
+console.log(combinedNames); 
