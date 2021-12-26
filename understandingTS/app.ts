bff070bf12b916c08672c9a5d4c2cd1ad3864dee
Debora Schuch da Rosa <StarAmbients@gmail.com>
2021-12-26 17:12:11 +0100

    Aliases

--- understandingTS/app.ts
+++ understandingTS/app.ts
@@ -17,7 +17,12 @@ const combinedNames = combine('Sarah', 'Richard');
 console.log(combinedNames); */
 
 // literal types
-function combine (input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text' ) {
+// type aliases
+
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text'; 
+
+function combine (input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor) {
     let result;
     if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
         result = +input1 + +input2;
