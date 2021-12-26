fa766420b23c5f1118738c007454227d3d826a6e
Debora Schuch da Rosa <StarAmbients@gmail.com>
2021-12-26 17:28:44 +0100

    Function types

--- understandingTS/app.ts
+++ understandingTS/app.ts
@@ -1,47 +1,17 @@
-// union types
-/* 
-function combine (input1: number | string, input2: number | string) {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
-    } else {
-            result = input1.toString() + input2.toString();
-    }  
-    return result;
+function add(n1: number, n2: number): number {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26);
-console.log(combinedAges);
-
-const combinedNames = combine('Sarah', 'Richard');
-console.log(combinedNames); */
-
-// literal types
-// type aliases
-
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text'; 
-
-function combine (input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor) {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    } else {
-            result = input1.toString() + input2.toString();
-    }  
-    return result;
-/*     if (resultConversion === 'as-number') {
-        return +result;
-    } else {
-        return result.toString();
-    } */
+function printResult(num: number): void {
+  console.log("Result...: " + num);
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
+printResult(add(5, 12));
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
+// let someValue: undefined; --> never use undefined as type of a function
 
-const combinedNames = combine('Sarah', 'Richard', 'as-text');
-console.log(combinedNames); 
+// let combinedValues: Function;
+let combinedValues: (a: number, b: number) => number;
+
+combinedValues = add;
+console.log(combinedValues(8, 70));
